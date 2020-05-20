8daa131f9b43e24bc3729a017a36c1866603a4bf
Rafael Filho <rafaelofil@outlook.com>
2020-05-20 17:41:31 -0300

    scroll-down-with-scroll-event

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -1,16 +1,1 @@
-export default function initScroll() {
-  const buttonScroll = document.querySelectorAll("#scroll-smooth");
-
-  function scrollToIntro(event) {
-    event.preventDefault(0);
-    const intro = document.querySelector("#intro");
-    intro.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  }
-
-  buttonScroll.forEach((link) => {
-    link.addEventListener("click", scrollToIntro);
-  });
-}
+export default function initScroll() {}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initAccordion from "./modules/accordion.js";
+import initScrollButton from "./modules/scrollButton.js";
 import initScroll from "./modules/scroll.js";
 
 initScroll();
+initScrollButton();
 initAccordion();
