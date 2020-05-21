670b356199f57a48d2e32f213907e2f6a1c86a23
Rafael Filho <rafaelofil@outlook.com>
2020-05-21 16:24:07 -0300

    scroll up - event

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -2,25 +2,52 @@ export default function initScroll() {
   const main = document.getElementById("main");
   const intro = document.getElementById("intro");
   const buttonScroll = document.getElementById("scroll-smooth");
+  let scrollToDown = false;
+
+  AddScrollEvent(() => {
+    window.addEventListener("scroll", directionScroll);
+  });
+  RemoveScrollEvent(() => {
+    window.removeEventListener("scroll", directionScroll);
+  });
+  buttonScroll.addEventListener("click", scrollToIntro);
 
   function directionScroll() {
     const mainTop = main.getBoundingClientRect().top;
     const mainBottom = main.getBoundingClientRect().bottom;
-    if (mainTop < 0 && mainBottom > 0) {
+
+    if (mainTop < 0 && !scrollToDown) {
       scrollToIntro();
+    } else if (mainBottom > 0 && scrollToDown) {
+      scrollUp();
     }
   }
 
   function scrollToIntro() {
-    window.removeEventListener("scroll", directionScroll);
+    RemoveScrollEvent();
 
     const introTop = intro.offsetTop;
     window.scrollTo({
       top: introTop,
       behavior: "smooth",
     });
+
+    setTimeout(() => {
+      scrollToDown = true;
+      AddScrollEvent();
+    }, 850);
   }
 
-  window.addEventListener("scroll", directionScroll);
-  buttonScroll.addEventListener("click", scrollToIntro);
+  function scrollUp() {
+    RemoveScrollEvent();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    setTimeout(() => {
+      scrollToDown = false;
+      AddScrollEvent();
+    }, 850);
+  }
 }
