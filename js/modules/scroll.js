4197819b4acd9ec4f5538e92ecf9f9f84d91f9a3
Rafael Filho <rafaelofil@outlook.com>
2020-05-21 13:25:35 -0300

    event scroll down

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -1,1 +1,26 @@
-export default function initScroll() {}
+export default function initScroll() {
+  const main = document.getElementById("main");
+  const intro = document.getElementById("intro");
+  const buttonScroll = document.getElementById("scroll-smooth");
+
+  function directionScroll() {
+    const mainTop = main.getBoundingClientRect().top;
+    const mainBottom = main.getBoundingClientRect().bottom;
+    if (mainTop < 0 && mainBottom > 0) {
+      scrollToIntro();
+    }
+  }
+
+  function scrollToIntro() {
+    window.removeEventListener("scroll", directionScroll);
+
+    const introTop = intro.offsetTop;
+    window.scrollTo({
+      top: introTop,
+      behavior: "smooth",
+    });
+  }
+
+  window.addEventListener("scroll", directionScroll);
+  buttonScroll.addEventListener("click", scrollToIntro);
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 import initAccordion from "./modules/accordion.js";
-import initScrollButton from "./modules/scrollButton.js";
 import initScroll from "./modules/scroll.js";
 
 initScroll();
-initScrollButton();
 initAccordion();
