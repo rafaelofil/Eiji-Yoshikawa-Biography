06898d28bd06ec919db9807c81b37e3ab531d4a0
Rafael Filho <rafaelofil@outlook.com>
2020-05-22 15:22:14 -0300

    scroll animation

--- js/modules/scrollAnimation.js
+++ js/modules/scrollAnimation.js
@@ -1,4 +1,17 @@
-// export default function initScrollAnimation() {
-//   const img = document.querySelector(".img-principal");
-//   const
-// }
+export default function initScrollAnimation() {
+  const sections = document.querySelectorAll("[data-anime='scroll']");
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.8;
+
+    function scrollAnime() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf < 0;
+        if (isSectionVisible) section.classList.add("active");
+        else section.classList.remove("remove");
+      });
+    }
+    scrollAnime();
+    window.addEventListener("scroll", scrollAnime);
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import initAccordion from "./modules/accordion.js";
 import initScroll from "./modules/scroll.js";
-// import initScrollAnimation from "./modules/scrollAnimation.js";
+import initScrollAnimation from "./modules/scrollAnimation.js";
 
 initScroll();
-// initScrollAnimation();
+initScrollAnimation();
 initAccordion();
