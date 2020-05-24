9fcbeb33930dbdc8cc132ede39ce8405f91b3b64
Rafael Filho <rafaelofil@outlook.com>
2020-05-24 16:29:00 -0300

    gulp-autoprefixer add

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 function compileSass() {
-  return gulp.src("css/sass/*.scss").pipe(sass()).pipe(gulp.dest("./css"));
+  return gulp
+    .src("css/sass/*.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 version"],
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest("./css"));
 }
 
 gulp.task("default", compileSass);
