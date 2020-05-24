34d94754dd96e2d3bd8ca42f0c8f043451c55263
Rafael Filho <rafaelofil@outlook.com>
2020-05-24 18:17:56 -0300

    language change

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -3,10 +3,12 @@ export default function initModal() {
   const buttonClose = document.querySelector("[data-modal='close']");
   const containerModal = document.querySelector("[data-modal='container']");
 
-  // const englishOption = document.querySelector("[data-modal='english']");
-  // const portugueseOption = document.querySelector("[data-modal='portuguese']");
-  // const english = document.querySelectorAll(".english");
-  // const portuguese = document.querySelectorAll(".portuguese");
+  const englishOption = document.querySelectorAll("[data-modal='english']");
+  const portugueseOption = document.querySelectorAll(
+    "[data-modal='portuguese']"
+  );
+  const english = document.querySelectorAll(".english");
+  const portuguese = document.querySelectorAll(".portuguese");
 
   function openModal() {
     containerModal.classList.add("active");
@@ -20,23 +22,36 @@ export default function initModal() {
     if (event.target === this) closeModal();
   }
 
-  // function englishActive(event) {
-  //   event.preventDefault();
-  //   english.forEach(classList.add("active"));
-  //   portuguese.forEach(portuguese.classList.remove("active"));
-  //   closeModal();
-  // }
+  function englishActive(event) {
+    event.preventDefault();
+    english.forEach((item) => {
+      item.classList.add("active");
+    });
+    portuguese.forEach((item) => {
+      item.classList.remove("active");
+    });
+    closeModal();
+  }
 
-  // function portugueseActive(event) {
-  //   portuguese.forEach(classList.add("active"));
-  //   english.forEach(classList.remove("active"));
-  //   closeModal();
-  // }
+  function portugueseActive(event) {
+    event.preventDefault();
+    portuguese.forEach((item) => {
+      item.classList.add("active");
+    });
+    english.forEach((item) => {
+      item.classList.remove("active");
+    });
+    closeModal();
+  }
 
   buttonOpen.addEventListener("click", openModal);
   buttonClose.addEventListener("click", closeModal);
   containerModal.addEventListener("click", clickOutModal);
 
-  // englishOption.addEventListener("click", englishActive);
-  // portugueseOption.addEventListener("click", portugueseActive);
+  englishOption.forEach((item) => {
+    item.addEventListener("click", englishActive);
+  });
+  portugueseOption.forEach((item) => {
+    item.addEventListener("click", portugueseActive);
+  });
 }
